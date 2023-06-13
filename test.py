ecfc9923d0d354d6b5b170a9272e6559536307b4
João Pedro Reis <jp_reis@id.uff.br>
2023-06-13 14:52:26 -0300

    Modelagem wavelet

---
+++ test.py
@@ -0,0 +1,11 @@
+import sys
+
+#def main(a):
+print("Arguments,", len(sys.argv))
+print("Args list", str(sys.argv))
+
+a = sys.argv[1]
+
+print(a)  
+#if __name__ == '__main__':
+#    main(sys.argv[1:])
